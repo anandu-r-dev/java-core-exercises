94c8420e6091848599e0b24158fe91c4cea44256
Anandu R <anandur78@gmail.com>
2026-05-19 10:25:35 +0530

    Even Odd Checker

---
+++ EvenOddChecker.java
@@ -0,0 +1,11 @@
+public class EvenOddChecker {
+    public static void main(String[] args) {
+        int number = 13;
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is an Even number.");
+        } else {
+            System.out.println(number + " is an Odd number.");
+        }
+    }
+}
